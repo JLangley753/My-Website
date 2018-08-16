1bb92b82bb3d97e3c96edf897356f54ef2dae23b
JLangley753 <42213489+JLangley753@users.noreply.github.com>
2018-08-16 16:15:43 +0100

    Amended animations
    Amended scrolltop distances to ensure it works well on different devices. Wrapped bullet classes in bullet ID div. Added basic responsiveness with regards to bullet and gallery.

--- main.js
+++ main.js
@@ -8,40 +8,53 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+  if (n > x.length) {
+    slideIndex = 1
   }
-  x[slideIndex-1].style.display = "block";
+  if (n < 1) {
+    slideIndex = x.length
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 };
 
 window.onscroll = function() {
   secASlide();
   secBSlide();
   secCSlide();
+  showBullet();
 };
 
 function secASlide() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-document.getElementById('secATab').className = "section-a tabs secaactive";
-} else {
-  document.getElementById('secATab').className = 'section-a tabs';
-}
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById('secATab').className = "section-a tabs secaactive";
+  } else {
+    document.getElementById('secATab').className = 'section-a tabs';
+  }
 };
 
 function secBSlide() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-document.getElementById('secBTab').className = "section-b tabs secbactive";
-} else {
-  document.getElementById('secBTab').className = 'section-b tabs';
-}
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    document.getElementById('secBTab').className = "section-b tabs secbactive";
+  } else {
+    document.getElementById('secBTab').className = 'section-b tabs';
+  }
 };
 
 function secCSlide() {
-  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-document.getElementById('secCTab').className = "section-c tabs seccactive";
-} else {
-  document.getElementById('secCTab').className = 'section-c tabs';
-}
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById('secCTab').className = "section-c tabs seccactive";
+  } else {
+    document.getElementById('secCTab').className = 'section-c tabs';
+  }
+};
+
+function showBullet() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    document.getElementById('bullet').className = "bulletactive";
+  } else {
+    document.getElementById('bullet').className = 'bullet';
+  }
 };
