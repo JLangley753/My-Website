dd307bad85ff07e0235de5f88a4a1f183867f191
JLangley753 <42213489+JLangley753@users.noreply.github.com>
2018-10-15 09:51:09 +0100

    Added noughts and crosses flat build

---
+++ build/precache-manifest.5e672b071232e872c871039683e7b7e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7397dafe944df22b80cd",
+    "url": "/static/js/main.7397dafe.chunk.js"
+  },
+  {
+    "revision": "36afbc2ff7a701f5e8ee",
+    "url": "/static/js/1.36afbc2f.chunk.js"
+  },
+  {
+    "revision": "7397dafe944df22b80cd",
+    "url": "/static/css/main.b4b5ebc1.chunk.css"
+  },
+  {
+    "revision": "762285e07469d95c387f3e1c5430f2f5",
+    "url": "/index.html"
+  }
+];
